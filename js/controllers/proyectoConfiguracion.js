2b528c4477a47f24f7725401497d217c6ba6f682
Sebastian Guevara <rlxsebas@hotmail.com>
2016-08-21 17:03:31 -0500

    Wizard Step from Configuration Model

--- js/controllers/proyectoConfiguracion.js
+++ js/controllers/proyectoConfiguracion.js
@@ -3,11 +3,12 @@
  */
 angular.module('asideMenuDemo')
     .controller('configuracionCtrl',
-        function(users, $scope,typeUser,$window,$http) {
+        function(users, $scope,typeUser,$window,$http,Friend) {
 
           // $scope.proyecto=fileTojson.get();
 
                 //$window.alert(JSON.stringify($scope.proyecto, null, 4));
-
-                $http.get('json/stores.json');
+                Friend.get().then(function (msg) {
+                        $scope.msg = msg;
+                });
         });
