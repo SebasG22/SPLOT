472f647a60f0787e94f824425016a935f68de59c
Sebastian Guevara <rlxsebas@hotmail.com>
2016-09-18 07:52:50 -0500

    User can sign in with github account: Only user who are in whitelist

--- js/controllers/login.js
+++ js/controllers/login.js
@@ -4,7 +4,7 @@
 
 angular.module('multiSplot')
     .controller('loginCtrl',
-        function (auth, $state, $scope, $window, whitelist, userActual, $firebaseArray, user, typeUser, menu) {
+        function (auth, $state, $scope, $window, whitelist, userActual, $firebaseArray, $firebaseObject, user, users, typeUser, menu) {
 
             this.login = function () {
                 auth.$signInWithEmailAndPassword($scope.login, $scope.password)
@@ -134,98 +134,243 @@ angular.module('multiSplot')
             $scope.whitelist = whitelist;
 
 
-            $scope.openButton=function () {
-
-             $scope.infoToken="Abri el boton";
-
-             var provider = new firebase.auth.GithubAuthProvider();
-             // [END createprovider]
-             // [START addscopes]
-             provider.addScope('repo');
-             // [END addscopes]
-             // [START signin]
-             firebase.auth().signInWithRedirect(provider);
+            $scope.initApp2=function () {
+                var provider = new firebase.auth.GithubAuthProvider();
+                //$window.alert(JSON.stringify(provider, null, 4));
 
 
-             firebase.auth().getRedirectResult().then(function (result) {
-             if (result.credential) {
-             // This gives you a GitHub Access Token. You can use it to access the GitHub API.
-             var token = result.credential.accessToken;
-             // [START_EXCLUDE]
-             $scope.infoToken=token;
-             $window.alert("Entre al IF Token");
-             } else {
-             $scope.infoToken="";
-             $window.alert("Entre al ELSE Token");
+                firebase.auth().signInWithPopup(provider).then(function(result) {
+                    // This gives you a GitHub Access Token. You can use it to access the GitHub API.
+                    var token = result.credential.accessToken;
 
-             // [END_EXCLUDE]
-             }
-             // The signed-in user info.
-             var user = result.user;
-
-                 if($scope.verWH(user.email)==true){
-                     $window.alert("Usuario autenticado con GitHub correctamente");
-                     $state.go("inicio.bienvenida");
-                 }
-
-             $window.alert("Usuario: "+user);
-
-             }).catch(function (error) {
-             // Handle Errors here.
-             var errorCode = error.code;
-             var errorMessage = error.message;
-             // The email of the user's account used.
-             var email = error.email;
-             // The firebase.auth.AuthCredential type that was used.
-             var credential = error.credential;
-             // [START_EXCLUDE]
-             if (errorCode === 'auth/account-exists-with-different-credential') {
-             $window.alert('You have already signed up with a different auth provider for that email.');
-             // If you are using multiple auth providers on your app you should handle linking
-             // the user's accounts here.
-             } else {
-             console.error(error);
-             }
-
-             });
-
-
-                //SE DEBE AGREGAR EL USUARIO A USER Y ELIMINARLO DE WHITELIST Y YA ENTRA AL SISTEMA
-             firebase.auth().onAuthStateChanged(function(user) {
-             if (user) {
-             // User is signed in.
-             var displayName = user.displayName;
-             var email = user.email;
-             var emailVerified = user.emailVerified;
-             var photoURL = user.photoURL;
-             var isAnonymous = user.isAnonymous;
-             var uid = user.uid;
-             var providerData = user.providerData;
-             // [START_EXCLUDE]
-
-             //document.getElementById('quickstart-account-details').textContent = JSON.stringify(user, null, '  ');
-             console.log("%O",user);
-             // [END_EXCLUDE]
-             } else {
-             // User is signed out.
-             // [START_EXCLUDE]
-             //document.getElementById('quickstart-sign-in-status').textContent = 'Signed out';
-             //document.getElementById('quickstart-sign-in').textContent = 'Sign in with GitHub';
-             //document.getElementById('quickstart-account-details').textContent = 'null';
-             //document.getElementById('quickstart-oauthtoken').textContent = 'null';
-             // [END_EXCLUDE]
-             }
-             // [START_EXCLUDE]
-             //document.getElementById('quickstart-sign-in').disabled = false;
-             // [END_EXCLUDE]
-             });
-             // [END authstatelistener]
-
-             };
+                    // The signed-in user info.
+                    var user = result.user;
+                    /*$window.alert("User:"+user);
+                     console.log("USER");
+                     console.log("%O",user);
+                     $window.alert(user.email); */
+                    $scope.userEmail=user.email;
+                    $scope.uid=user.uid;
 
 
 
-             //Metodos del Login
+                    var myvalue= false;
+                    angular.forEach($scope.whitelist, function (value, key) {
+
+                        if(value.correo==$scope.userEmail){
+
+                            $scope.userFounded=value;
+
+
+
+                            myvalue=true;
+                        }
+
+                    });
+
+
+
+                    if(myvalue==true){
+                        console.log("Usuario Encontrado: Redireccionando a Inicio");
+                        //$scope.agregarUser($scope.uid,$scope.userFounded);
+
+                        // add it to the /users array
+                        // using the same uid defined by firebase
+                        var userRef = users.$ref().child($scope.uid);
+                        var newUser = $firebaseObject(userRef);
+                        newUser.uid =$scope.uid;
+                        newUser.usuario = $scope.userFounded.correo;
+                        newUser.nombre= $scope.userFounded.nombre;
+                        newUser.identificacion=$scope.userFounded.identificacion;
+                        newUser.direccion=$scope.userFounded.direccion;
+                        newUser.profesion=$scope.userFounded.profesion;
+                        newUser.tipo=$scope.userFounded.tipo;
+                        newUser.activo='true';
+                        newUser.imagen=$scope.userFounded.imagen;
+
+
+
+                        // save
+                        newUser.$save()
+                            // if everything is ok
+                            .then( function(ref) {
+                                // report in console
+                                console.log("User " + $scope.uid+ " added to the users list!");
+                                console.log("Se ha agregado el usuario correctamente a Users");
+                                userActual.setUID($scope.uid);
+                                userActual.set($scope.userFounded);
+
+
+
+                                //Reference to menu branch in Firebase
+                                var ref = firebase.database().ref("sistema/menuGeneral");
+
+
+                                //Compare the user type
+                                if ($scope.userFounded.tipo == "Administrador") {
+
+                                    //Update the typeUser Factory
+                                    typeUser.set("Administrador");
+
+
+                                    //Search the "Administador" menu
+                                    ref.orderByKey().equalTo("Administrador").on("child_added", function (snapshot) {
+
+                                        //Get the menu
+                                        $scope.menuSearched = snapshot.val();
+
+                                        //Print the object
+                                        //$window.alert(JSON.stringify($scope.menuSearched, null, 4));
+
+                                        //Update the data menu in the factory
+                                        menu.set($scope.menuSearched);
+
+                                        //Go to Welcome Page
+                                        $state.go("inicio.bienvenida");
+
+
+                                    });
+
+
+                                }
+
+                                else if ($scope.userFounded.tipo == "Lider") {
+
+                                    //Update the typeUser Factory
+                                    typeUser.set("Lider");
+
+
+                                    //Search the "Administador" menu
+                                    ref.orderByKey().equalTo("Lider").on("child_added", function (snapshot) {
+
+                                        //Get the menu
+                                        $scope.menuSearched = snapshot.val();
+
+                                        //Print the object
+                                        //$window.alert(JSON.stringify($scope.menuSearched, null, 4));
+
+                                        //Update the data menu in the factory
+                                        menu.set($scope.menuSearched);
+
+                                        //Go to Welcome Page
+                                        $state.go("inicio.bienvenida");
+
+
+                                    });
+
+                                }
+                                else if ($scope.userFounded.tipo == "Configurador") {
+
+                                    //Update the typeUser Factory
+                                    typeUser.set("Configurador");
+
+
+                                    //Search the "Administador" menu
+                                    ref.orderByKey().equalTo("Configurador").on("child_added", function (snapshot) {
+
+                                        //Get the menu
+                                        $scope.menuSearched = snapshot.val();
+
+                                        //Print the object
+                                        //$window.alert(JSON.stringify($scope.menuSearched, null, 4));
+
+                                        //Update the data menu in the factory
+                                        menu.set($scope.menuSearched);
+
+                                        //Go to Welcome Page
+                                        $state.go("inicio.bienvenida");
+
+                                    });
+
+                                }
+
+
+                                // $state.go('inicio.bienvenida');
+
+                                // the promise will return ok
+                            }, function(error){
+                                // report in console
+                                console.error("User " + $scope.uid + " cannot be added to the users list!");
+                                // the promise will produce an error
+                            });
+
+
+
+
+
+                    }
+
+                    // ...
+                }).catch(function(error) {
+                    // Handle Errors here.
+                    var errorCode = error.code;
+                    var errorMessage = error.message;
+                    // The email of the user's account used.
+                    var email = error.email;
+                    // The firebase.auth.AuthCredential type that was used.
+                    var credential = error.credential;
+                    // ...
+                });
+
+
+                firebase.auth().getRedirectResult().then(function(result) {
+                    if (result.credential) {
+                        // This gives you a GitHub Access Token. You can use it to access the GitHub API.
+                        var token = result.credential.accessToken;
+                        // ...
+                    }
+                    // The signed-in user info.
+                    var user = result.user;
+
+                }).catch(function(error) {
+                    // Handle Errors here.
+                    var errorCode = error.code;
+                    var errorMessage = error.message;
+                    // The email of the user's account used.
+                    var email = error.email;
+                    // The firebase.auth.AuthCredential type that was used.
+                    var credential = error.credential;
+                    // ...
+                });
+
+            };
+
+
+
+            $scope.agregarUser = function (uid,usuario) {
+                // add it to the /users array
+                // using the same uid defined by firebase
+                var userRef = users.$ref().child(uid);
+                var newUser = $firebaseObject(userRef);
+                newUser.uid =uid;
+                newUser.usuario = usuario.correo;
+                newUser.nombre= usuario.nombre;
+                newUser.identificacion=usuario.identificacion;
+                newUser.direccion=usuario.direccion;
+                newUser.profesion=usuario.profesion;
+                newUser.tipo=usuario.permiso;
+                newUser.activo='true';
+                newUser.imagen=usuario.imagen;
+
+
+
+                // save
+                newUser.$save()
+                    // if everything is ok
+                    .then( function(ref) {
+                        // report in console
+                        console.log("User " + uid+ " added to the users list!");
+                        // the promise will return ok
+                    }, function(error){
+                        // report in console
+                        console.error("User " + uid + " cannot be added to the users list!");
+                        // the promise will produce an error
+                    });
+            };
+
+
+
+            //Metodos del Login
 
             //Método que verifica si el usuario esta en la WH
             $scope.verWH = function(correo) {
@@ -265,28 +410,12 @@ angular.module('multiSplot')
 
                 else {
                     console.log("Usuario No encontrado");
-                    $window.alert("No tiene invitación para registrarse");
+                    $window.alert("Usuario no encontrado: No tiene invitación para registrarse");
                     return false;
                 }
 
             };
 
-            $scope.agregarUsuario = function() {
-
-
-                // create the user
-                userService.createUser($scope.login,$scope.password, $scope.nombre, $scope.identificacion, $scope.direccion, $scope.profesion, $scope.permiso,$scope.imagen)
-                    // if everything is ok
-                    .then(function (user) {
-                        $scope.whitelist.$remove($scope.userFounded);
-                        $state.go("inicio.listarUsuarios");
-                    })
-                    // if there is an error
-                    .catch(function (error) {
-                        $window.alert(error);
-                    });
-
-
-            };
         }
     );
+
