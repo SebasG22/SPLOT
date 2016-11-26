bbaf96db72c913d8cb0294bd83a24cf7c65126ec
Sebastian Guevara <rlxsebas@hotmail.com>
2016-11-26 09:24:18 -0500

    Removing the user - Deleting the user from the DataBase

--- modules/users/userInactive.js
+++ modules/users/userInactive.js
@@ -6,10 +6,13 @@ angular.module('usersSplot')
         function (users, $scope, typeUser, $window, userUpdate, $state, auth, projects, $firebaseArray) {
 
             var user = firebase.auth().currentUser;
+            $scope.usuarios=users;
 
             $scope.goOut= function () {
                 user.delete().then(function () {
                     // User deleted.
+
+                    $scope.usuarios.$remove(user);
                     $state.go("login");
                 }, function (error) {
                     // An error happened.
